dd565383586aed8649fdb150a202975f49bb5ca3
Kovács Vince <vincekovacs@hotmail.com>
2015-03-03 01:55:02 +0100

    Added to each log entry created date and user

--- src/ViKon/DbLog/Type/AbstractLogEntry.php
+++ src/ViKon/DbLog/Type/AbstractLogEntry.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace ViKon\DbLog\Type;
+
+use Carbon\Carbon;
 use ViKon\DbLog\Models\Log;
 
 /**
@@ -11,14 +13,12 @@ use ViKon\DbLog\Models\Log;
  * @package ViKon\DbLog
  */
 abstract class AbstractLogEntry {
-    /** @var null|\ViKon\DbLog\Models\Log */
-    protected $log = null;
-
     /** @var string */
     protected $name;
-
     /** @var array */
     protected $data = [];
+    /** @var null|\ViKon\DbLog\Models\Log */
+    private $log = null;
 
     /**
      * @param string                       $name
@@ -26,12 +26,41 @@ abstract class AbstractLogEntry {
      */
     public function __construct($name, Log $log = null) {
         $this->name = $name;
-        $this->log = $log;
+        if ($this->log !== null) {
+            $this->log = $log;
+            $this->data = $this->log->data;
+        }
+    }
+
+    /**
+     * @return null|\ViKon\Auth\Models\User
+     */
+    public function createdByUser() {
+        if ($this->log) {
+            return $this->log->createdByUser;
+        }
+
+        return null;
+    }
+
+    /**
+     * @return \Carbon\Carbon|null
+     */
+    public function createdAt() {
+        if ($this->log) {
+            return $this->log->created_at;
+        }
+
+        return null;
     }
 
     public function save() {
         if ($this->log === null) {
             $this->log = new Log();
+            $this->log->created_at = new Carbon();
+            if (\Auth::check()) {
+                $this->log->created_by_user_id = \Auth::user()->id;
+            }
         }
 
         $this->log->name = $this->name;
